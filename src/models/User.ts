62a62e6ee22d78f54328743cc451255a61920385
Lia <juliareinaldi@outlook.com>
2020-12-22 10:50:05 -0300

    first commit

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column('time with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
